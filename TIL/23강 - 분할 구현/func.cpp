44c8caae4d37f55ec438aaadcd1e4da97bf4034e
승환 지 <horong0602@outlook.com>
2024-11-02 21:23:25 +0900

    TIL: 분할 구현: 헤더, 컴파일, 링크

--- TIL/23강 - 분할 구현/func.cpp
+++ TIL/23강 - 분할 구현/func.cpp
@@ -1,4 +1,4 @@
-#include "func.h" // 전처리기 구문, 'func.h' 헤더 파일을 불러온다.
+#include "func.h" // 전처리기 구문, 'func.h' 헤더 파일을 불러온다.(헤더 파일의 내용을 복붙하는 개념)
 // =
 // int Add(int a, int b);
 // int Sub(int a, int b);
