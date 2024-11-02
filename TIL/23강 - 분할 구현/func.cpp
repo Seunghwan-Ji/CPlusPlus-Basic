176ffb45217a22dec4abb24ddb3568dc0a5985e2
승환 지 <horong0602@outlook.com>
2024-11-02 20:25:13 +0900

    TIL: 분할 구현, 컴파일과 링크

---
+++ TIL/23강 - 분할 구현/func.cpp
@@ -0,0 +1,20 @@
+#include "func.h" // 전처리기 구문, 'func.h' 헤더 파일을 불러온다.
+// =
+// int Add(int a, int b);
+// int Sub(int a, int b);
+// int Mul(int a, int b);
+
+int Add(int a, int b)
+{
+	return a + b;
+}
+
+int Sub(int a, int b)
+{
+	return a - b;
+}
+
+int Mul(int a, int b)
+{
+	return a * b;
+}
