bde5589e6eeacb18d2747f96e3dcc40cc17f630c
Seunghwan-Ji <horong0602.dev@gmail.com>
2024-11-13 19:00:29 +0900

    새 커밋 메시지

---
+++ TIL/44강 - 가변 배열(2)/Arr.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// 가변 배열 기능을 갖춘 자료형, 이 자료형으로 생성한 변수 객체들로 가변배열 기능을 사용하기 위한 목적이다.
+typedef struct _tabArr
+{
+	int* pInt; // 데이터를 받아 저장할 때, 자료형의 크기 자체가 한계가 있기 때문에, 힙 메모리의 시작 주소를 받는 용도.
+	int  iCount; // 데이터를 받을 때마다 힙 메모리 주소로 접근했을 때,
+	// 시작 주소로부터 몇번째 만큼 떨어진 위치에 저장해야 하는지 참조하기 위해 데이터를 받은 횟수를 기록하는 용도.
+	int  iMaxCount; // 힙 메모리에 데이터가 모두 채워졌을 때 한계치를 늘리기 위해,
+	// 힙 메모리의 전체 칸 수를 기록하여 iCount 와 비교하는 용도.
+}tArr;
+
+// 가변 배열 초기화 함수
+void InitArr(tArr* _pArr); // 초기화만 하기에 반환값은 필요하지 않으므로, void 타입.
+
+// 데이터 추가 함수
+void PushBack(tArr* _pArr, int _iData); // 추가할 데이터를 인자로 받도록 한다.
+
+// 공간 추가 확장 함수
+void Reallocate(tArr* _pArr);
+
+// 힙 메모리 해제 함수
+void ReleaseArr(tArr* _pArr);
