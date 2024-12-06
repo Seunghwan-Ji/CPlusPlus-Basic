4e5b53894a966780a635d4a0f238136d83ea9fdb
Seunghwan-Ji <horong0602.dev@gmail.com>
2024-12-07 00:54:26 +0900

    새 커밋 메시지

---
+++ TIL/71강 - tree(3)/round71.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map> // 레드블랙 이진탐색트리가 구현되어 있는 자료구조.
+
+using std::cout;
+using std::endl;
+
+using std::map;
+using std::make_pair;
+
+int main()
+{
+	// 이진탐색
+	// 1. 정렬되어 있는 데이터에 적용 가능.
+
+	// 이진탐색트리
+	// 1. 이진탐색을 사용할 수 있게 고안된 이진트리.
+	// 2. 데이터 입력 시 O(logN) 효율.
+	// 3. 탐색 효율은 O(logN)
+	// 4. 트리의 모양이 밸런스가 유지되지 않으면 제대로 된 탐색 효율이 나오지 않는다.
+	//    - 자가균형 기능 필요(AVL, Red/Black Tree)
+
+	return 0;
+}
